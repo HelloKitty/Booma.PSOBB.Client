1d36960a031c55f32aa9dc1ea4f4ab779fe9d00e
HelloKitty <andrew.blakely@ymail.com>
2019-01-31 12:27:36 -0600

    Removed a random build in the directory

---
+++ Assets/Content/TestBehaviour1.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Threading.Tasks;
+
+public class TestBehaviour1 : MonoBehaviour {
+
+	// Use this for initialization
+	async Task Start () {
+		await Task.Delay(3000);
+		Debug.Log("Finished start");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log("Running Update");
+	}
+}
